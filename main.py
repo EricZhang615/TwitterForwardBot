d113919cfd98cbbe8ee51579a82285a5601a4baa
EricZhang615 <eric20000615@gmail.com>
2021-09-20 21:40:25 +0800

    debug

--- main.py
+++ main.py
@@ -61,7 +61,9 @@ class TwitterUser(twint.config.Config):
 
 def pull_and_send(user: TwitterUser):
     user.get_recent_tweet()
+    print(user.recentTweet)
     user.get_new_tweet()
+    print(user.newTweet)
     for m in user.message_list():
         bot.send_message(p["chat_id"], m)
 
